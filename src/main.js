da4bbbbe0d6a97655667ed2e2814f37425d2e4f8
Nio <zhang.tao@decathlon.com>
2025-06-06 16:46:24 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,16 +1,16 @@
 import { createApp } from "vue";
 import "./style.css";
-import ElementPlus from "element-plus";
-import "element-plus/dist/index.css";
-import ArcoVue from "@arco-design/web-vue";
-import "@arco-design/web-vue/dist/arco.css";
-import "remixicon/fonts/remixicon.css";
+// import ElementPlus from "element-plus";
+// import "element-plus/dist/index.css";
+// import ArcoVue from "@arco-design/web-vue";
+// import "@arco-design/web-vue/dist/arco.css";
+// import "remixicon/fonts/remixicon.css";
 import App from "./App.vue";
 import router from "@/router";
 
 const app = createApp(App);
 
-app.use(ElementPlus);
-app.use(ArcoVue);
+// app.use(ElementPlus);
+// app.use(ArcoVue);
 app.use(router);
 app.mount("#app");
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,38 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { resolve } from "path";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import {
+  ArcoResolver,
+  ElementPlusResolver,
+} from "unplugin-vue-components/resolvers";
+import { vitePluginForArco } from "@arco-plugins/vite-vue";
+import compression from "vite-plugin-compression";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [ArcoResolver(), ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [
+        ArcoResolver({
+          sideEffect: true,
+        }),
+        ElementPlusResolver(),
+      ],
+    }),
+    vitePluginForArco({
+      style: "css",
+    }),
+    compression({ algorithm: "gzip", deleteOriginFile: false }),
+  ],
+  esbuild: {
+    drop: ["console", "debugger"],
+  },
   base: process.env.NODE_ENV === "production" ? "/common-tools/" : "/",
   build: {
     rollupOptions: {
